6f824813454868ddedfe1a3fb2aa3c766a69ab5b
李英辉 <xiaohuidexinge@163.com>
2015-04-14 04:24:23 +0800

    init

---
+++ bird/Assets/Script/MenuBirdController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBirdController : MonoBehaviour {
+
+	public GameObject bird;
+	private float timer;
+	private float y;
+
+	// Use this for initialization
+	void Start () {
+		y = bird.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime * 8;
+		float dis = Mathf.Sin (timer)/14;
+		bird.transform.position = new Vector2( bird.transform.position.x, y + dis);
+
+	}
+}
